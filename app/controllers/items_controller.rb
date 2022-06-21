a883c56749092b7d179bd4177bbcbf05916b1c07
Zach <shaw.zacht@gmail.com>
2022-06-21 15:20:21 -0700

    commit tree 9bf48f48e2c719359247e0c86e3fa42f1cc88dea

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def render_not_found_response(invalid)
+    render json: { errors: "Item not found" }, status: :not_found
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
